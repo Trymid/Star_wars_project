e70ee85bc0fc810c47aca8738923f28ba79372b2
Trymid <48253230+Trymid@users.noreply.github.com>
2022-05-21 11:12:08 +0300

    Add files via upload
    Add button 'load more'

--- movie_page.js
+++ movie_page.js
@@ -1,5 +1,9 @@
 let wrapper = document.getElementById('wrapper');
-let main = document.getElementById('main');
+let currentDownloadedInfo = 0;
+let numberDownloadInfo = 10;
+let j = 0;
+let ol;
+
 
 getFilmData();
 
@@ -10,26 +14,28 @@ async function getFilmData() {
                     return response;
                   });
   filmData = filmData.data;
-  console.log(filmData)
+  // console.log(filmData)
   
   generateMoviePage(filmData);
 }
 
+
 function getCurrentFilmId() {
   let movieId = localStorage.getItem('movieId');
-  console.log(movieId)
+  // console.log(movieId)
   let filmDep = JSON.parse(localStorage.getItem('filmsDependencies'));
-  console.log(filmDep)
+  // console.log(filmDep)
   let result = filmDep[movieId];
-  console.log(result)
+  // console.log(result)
   return result;
 }
 
 
 
-async function generateMoviePage(film) {
+function generateMoviePage(film) {
   let movie = film;
-  wrapper.innerHTML = `<div class="main">
+  // console.log(movie)
+  wrapper.innerHTML = `<div id="main" class="main">
                         <img class="poster" src="./images/${movie.episode_id}.jpg" alt='poster'>
                         <h3 class="title">Star wars - ${movie.title}</h3>
                         <p class="episode_id">Episode: ${movie.episode_id}</p>
@@ -37,22 +43,51 @@ async function generateMoviePage(film) {
                         <p class="opening_crawl">${movie.opening_crawl}</p>
                       </div>`
   // console.log(movie.characters)
+  loadInfo(movie)
+}
+
+async function loadInfo(data) {
+  
+  let main = document.getElementById('main');
+  let movie = data;
+  console.log(movie)
   let charId = [];
   let adress = "https://star--wars.herokuapp.com/people/";
-
-  for (let i = 0; i < movie.characters.length; i++) {
-    charId[i] = adress + movie.characters[i].match(/\d+/).join('');
-  }
-  // console.log(charId)
+  let arrayIndex = 0;
   
-  let characters = await getCharacters(movie.characters);
-  // console.log(characters)
+  console.log(currentDownloadedInfo);
+      console.log(numberDownloadInfo);
+      console.log(j)
 
-  let charPicsLinks = await getcharPicsLinks(charId);
+  if (currentDownloadedInfo === 0) {
+    currentDownloadedInfo = numberDownloadInfo;
+    ol = document.createElement('ol');
+  }
+
+  if (movie.characters.length < currentDownloadedInfo) {
+    currentDownloadedInfo = movie.characters.length;
+  }
+
+  // if (movie.characters.length < currentDownloadedInfo) {
+  //   numberDownloadInfo = movie.characters.length;
+  //   currentDownloadedInfo = movie.characters.length;
+  // }
+
+  for (let i = j; i < currentDownloadedInfo; i++) { // movie.characters.length   numberDownloadInfo
+    
+    charId[arrayIndex] = adress + movie.characters[i].match(/\d+/).join('');
+    arrayIndex++;
+  }
+  arrayIndex = 0;
+  console.log(charId)
+
+  let characters = await getCharacters(movie.characters, j);
+  console.log(characters)
+
+  let charPicsLinks = await getCharPicsLinks(charId, j);
   console.log(charPicsLinks)
-  let ol = document.createElement('ol');
 
-  for (let i = 0; i < characters.length; i++) {
+  for (let i = 0; i < characters.length; i++) { // characters.length    currentDownloadedInfo
     let li = document.createElement('li');
     li.innerHTML = `<a href="${charPicsLinks[i].data.wiki}">
                       <p>${characters[i].data.name}</p>
@@ -61,16 +96,47 @@ async function generateMoviePage(film) {
                     `;
     ol.append(li);
   }
-  
   main.append(ol);
+
+  if (movie.characters.length > currentDownloadedInfo) {
+    let btnloadMore = document.createElement('button');
+    btnloadMore.setAttribute('id', 'btnloadMore');
+    btnloadMore.setAttribute('type', 'button');
+    btnloadMore.textContent = 'load more';
+    
+    ol.after(btnloadMore);
+
+    console.log(currentDownloadedInfo);
+    console.log(numberDownloadInfo);
+    console.log(j)
+
+    btnloadMore.addEventListener('click', () => {
+      j += numberDownloadInfo;
+      currentDownloadedInfo += numberDownloadInfo;
+      btnloadMore.remove();
+      loadMoreInfo(movie);
+      
+      console.log(`currentDownloadedInfo ` + currentDownloadedInfo);
+      console.log(`numberDownloadInfo ` + numberDownloadInfo);
+      console.log(`j ` + j)
+    });
+  }
+  
 }
 
-async function getCharacters(data) {
+
+function loadMoreInfo(data) {
+  loadInfo(data);
+}
+
+async function getCharacters(data, index) {
   let charactersLinks = data;
   let charactersPack = [];
+  let arrayIndex = 0;
 
-  for (let i = 0; i < charactersLinks.length; i++) {
-    charactersPack[i] = axios.get(charactersLinks[i]);
+  for (let i = index; i < currentDownloadedInfo; i++) { // charactersLinks.length  numberDownloadInfo
+    charactersPack[arrayIndex] = axios.get(charactersLinks[i]);
+    arrayIndex++;
   }
 
   let charactersDataPack = await axios.all(charactersPack)
@@ -82,10 +148,11 @@ async function getCharacters(data) {
 }
 
 
-async function getcharPicsLinks(data) {
+async function getCharPicsLinks(data, index) {
   let charList = data;
+  console.log(charList)
 
-  for (let i = 0; i < charList.length; i++) {
+  for (let i = 0; i < charList.length; i++) { //charList.length   numberDownloadInfo
     charList[i] = axios.get(charList[i]);
   }
 
